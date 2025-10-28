e69b16f7e0af5ab2b1ec04e4b0e79b2bb420b6d5
sanskar <sanskar.003.02@gmail.com>
2025-10-28 14:42:59 +0530

    updated APIs

--- frontend/src/slices/threadsSlice.js
+++ frontend/src/slices/threadsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL.replace(/\/+$/, "");
 
 // 🔹 Fetch all threads
 export const fetchThreads = createAsyncThunk(
